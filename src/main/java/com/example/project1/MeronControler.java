743f16548bd58ae80a4ae9f31a9ae17dbc79e517
Meron Zeru <meronomz@gmail.com>
2019-06-18 04:26:04 -0400

    first commit

---
+++ src/main/java/com/example/project1/MeronControler.java
@@ -0,0 +1,52 @@
+package com.example.project1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MeronControler {
+    @RequestMapping("/")
+    public String home(){
+        return "bloghome";
+    }
+    @RequestMapping("/gitHub")
+    public String gitHub() {
+
+        return "gitHub";
+    }
+
+    @RequestMapping("/programDesign")
+    public String programDesign() {
+        return "programDesign";
+    }
+
+    @RequestMapping("/coreJava")
+    public String coreJava() {
+        return "coreJava";
+    }
+
+    @RequestMapping("/oop")
+    public String oop() {
+        return "oop";
+    }
+
+    @RequestMapping("/hcb")
+    public String hcb() {
+        return "hcb";
+    }
+
+    @RequestMapping("/jbc")
+    public String jbc() {
+        return "jbc";
+    }
+
+    @RequestMapping("gitHub1")
+    public String gitHub1() {
+        return "gitHub1";
+    }
+
+    @RequestMapping("/profile")
+    public String profile() { return "profile";
+    }
+}
+
